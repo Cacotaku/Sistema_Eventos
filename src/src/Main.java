75215f8c41039266011aeebd947a6cd737834e14
Paulo de Tarso <paulobitas@gmail.com>
2025-10-04 13:52:48 -0300

    Atualizando classe

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 //Proposta da página
 //Criar uma chamada de opções
 			/*
@@ -34,25 +32,15 @@
 				 0 - * Sair
 */
 
-
-// 25/06/2025 ~ 09/07/2025 Projeto parado devido a problemas na estação de trabalho.
-//09/07/2025 Busca de usuários de um evento
-// (necessita de limpeza no código geral - tarefa para melhorar o código)
-// 16/07/2025 - Adicionado inscrição em um evento
-
 package src;
-
-import com.mysql.cj.protocol.Resultset;
 
 import javax.swing.*;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Main
 {
-
     public static String Opcoes()
     {
         return "Sistema de cadastros Baco\\n\n" +
@@ -71,111 +59,83 @@ public class Main
             "\t\t\t\t 0 - * Sair\n";
     }
 
-    public static void main(String [] args) throws SQLException, Exception {
-       SqlTest sqlTest = new SqlTest();
+    public static void main(String [] args) throws SQLException, Exception 
+    {
+       SqlTest sqlChamada = new SqlTest();
        SqlTest.SQLChamada();
 
+       String mensagem = Opcoes();
 
-        String mensagem = Opcoes();
-
-		int opt = Integer.parseInt(JOptionPane.showInputDialog(mensagem));
-        while(opt > 1 || opt < 7)
-        {
-            switch(opt)
-		    {
-                case 1: //  1 - * Busca de Eventos que o usuário Participou
+	   int opt = Integer.parseInt(JOptionPane.showInputDialog(mensagem));
+       while(opt > 1 || opt < 7)
+       {
+           switch(opt)
+		   {
+               case 1: //  1 - * Busca de Eventos que o usuário Participou
+                	             	
+                	int buscaUsuario = Integer.parseInt(
+                			JOptionPane.showInputDialog(null, "Digite o id do usuário buscado: "));
+                	SqlTest.BuscaEvUs(buscaUsuario);
+                	mensagem = "Deseja realizar mais alguma operação?\n" + Opcoes();
+                    opt = Integer.parseInt(
+                    		JOptionPane.showInputDialog(mensagem));                 	
                     break;
 
-                case 2: //  2 - * Busca de Eventos de uma categoria
+               case 2: //  2 - * Busca de Eventos de uma categoria
 
-                    System.out.println("Flag4 - Entrou em Listar Categorias");
+            	   int id_Categoria = Integer.parseInt(
+            			   JOptionPane.showInputDialog(null,"Digite o id da categoria: "));
+                   Categoria buscaEventoCategoria = new Categoria();
+	               buscaEventoCategoria.setId_Categoria(id_Categoria);	
+	               List<Categoria> listaEventos = new ArrayList<Categoria>();	
+	               listaEventos = sqlChamada.Buscar(buscaEventoCategoria);
+	               mensagem = "Deseja realizar mais alguma operação?\n" + Opcoes();
+	               opt = Integer.parseInt(
+	            		   JOptionPane.showInputDialog(mensagem));	
+	               break;
 
-                    int id_Categoria = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite o id da categoria: "));
+               case 3: //  3 - * Buscar usuários de um evento                    
 
-                    Categoria testee = new Categoria();
-
-                    testee.setId_Categoria(id_Categoria);
-
-                    List<Categoria> listaEventos = new ArrayList<Categoria>();
-
-                    listaEventos = sqlTest.Buscar(testee);
-
-                    System.out.println(listaEventos.toArray().length);
-
-                    mensagem = "Deseja realizar mais alguma operação?\n" + Opcoes();
-                    opt = Integer.parseInt(JOptionPane.showInputDialog(mensagem));
-
-
-                    break;
-
-                case 3: //  3 - * Buscar usuários de um evento
-
-                    System.out.println("Flag5 - Entrou em Listar usuários de um evento");
-
-                    int id_Evento = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite o id do evento: "));
-
+                    int id_Evento = Integer.parseInt(
+                    		JOptionPane.showInputDialog(null,"Digite o id do evento: "));
                     Evento_Usuario evento_usuario = new Evento_Usuario();
                     evento_usuario.setId_EveUsu(id_Evento);
-
                     List<Evento_Usuario> listaEvento_Usuario = new ArrayList<Evento_Usuario>();
-
-                    listaEvento_Usuario = sqlTest.Buscar(evento_usuario);
-
-                    System.out.println(listaEvento_Usuario.toArray().length);
-
+                    listaEvento_Usuario = sqlChamada.Buscar(evento_usuario);
                     mensagem = "Deseja realizar mais alguma operação?\n" + Opcoes();
-                    opt = Integer.parseInt(JOptionPane.showInputDialog(mensagem));
-
-
+                    opt = Integer.parseInt(
+                    		JOptionPane.showInputDialog(mensagem));
                     break;
 
-                case 4: //  4 - * Cadastrar um evento
-
-                    System.out.println("Flag1 - Entrou em cadastrar evento");
+               case 4: //  4 - * Cadastrar um evento
+            	   
                     Eventos eventos = new Eventos();
                     eventos.Registro();
                     mensagem = "Deseja realizar mais alguma operação?\n" + Opcoes();
                     opt = Integer.parseInt(JOptionPane.showInputDialog(mensagem));
-
                     break;
 
-                case 5: //  5 - * Cadastrar um usuário
-
-                    System.out.println("Flag2 - Entrou em cadastrar usuário");
+               case 5: //  5 - * Cadastrar um usuário
+                    
                     Usuario usuario = new Usuario();
                     usuario.Registro();
                     mensagem = "Deseja realizar mais alguma operação?\n" + Opcoes();
                     opt = Integer.parseInt(JOptionPane.showInputDialog(mensagem));
-
                     break;
 
-                case 6: //  6 - * Cadastrar Categorias
-
-                    System.out.println("Flag3 - Entrou em cadastrar categoria");
+                case 6: //  6 - * Cadastrar Categorias                    
 
                     Categoria categoria = new Categoria();
-
                     categoria.Registro();
-
                     mensagem = "Deseja realizar mais alguma operação?\n" + Opcoes();
-
                     opt = Integer.parseInt(JOptionPane.showInputDialog(mensagem));
-
-
                     break;
 
-                case 7: //  7 - * Cadastrar inscrição num evento
-
-                    System.out.println("Flag6 - Entrou em cadastrar usuario em um evento");
-
-                    Evento_Usuario e_u = new Evento_Usuario();
+                case 7: //  7 - * Cadastrar inscrição num evento                    
 
                     Evento_Usuario.Registro();
-
                     mensagem = "Deseja realizar mais alguma operação?\n" + Opcoes();
-
                     opt = Integer.parseInt(JOptionPane.showInputDialog(mensagem));
-
                     break;
 
                 case 0: //  0 - * Sair
@@ -185,10 +145,9 @@ public class Main
 
                     JOptionPane.showMessageDialog(null,"Opção Inválida\n");
                     mensagem = "Deseja realizar mais alguma operação?\n" + Opcoes();
-                    opt = Integer.parseInt(JOptionPane.showInputDialog(mensagem));
-
+                    opt = Integer.parseInt(
+                    		JOptionPane.showInputDialog(mensagem));
                     break;
-
 		    }
         }
 	}
